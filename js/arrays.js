3ee418c4886d5adc147875eeedbca18e8c150392
SuleeJK <silemanijangana@gmail.com>
2022-11-02 19:36:33 +0100

    .

--- js/arrays.js
+++ js/arrays.js
@@ -1,35 +1,47 @@
 // console.log("Proves amb arrays")
 
-// function demoArrays() {
+function demoArrays() {
 
-//     // Crear un array buid (llista)
-//     var persones = []
+    // Crear un array buid (llista)
+    var persones = []
 
-//     // Afegir elements a un array
-//     persones.push("Anna")
-//     persones.push("Eva")
+    // Afegir elements a un array
+    persones.push("Anna")
+    persones.push("Eva")
 
-//     //Creem un array i l'iniciem
-//     var alumnes = [
-//         "Pere",
-//         "Laura",
-//         "Dani",
-//         '<p style="color:blue">L\'altre</p>'
-//     ]
+    //Creem un array i l'iniciem
+    var alumnes = [
+        "Pere",
+        "Laura",
+        "Dani",
+        '<p style="color:blue">L\'altre</p>'
+    ]
 
-//     alumnes.push("Arnau")
-//     alumnes.pop()
-//     alumnes.push("Robert")
-//     console.log("L'element que s'ha eliminat és ", alumnes.shift())
+    alumnes.push("Arnau")
+    alumnes.pop()
+    alumnes.push("Robert")
+    console.log("L'element que s'ha eliminat és ", alumnes.shift())
 
-// console.log(persones,alumnes)
+    console.log(persones, alumnes)
 
-var numeros = [34, 12, 10, 2]
-var total = 0
+    var numeros = [34, 12, 10, 2]
+    var total = 0
+    var mitjana = 0
+    console.log("Abans del sumatori")
+    numeros.forEach(sumatori)
+    console.log("Després del sumatori")
+    console.log(total)
 
-numeros.forEach(sumatori)
+    function sumatori(valor, index, llista) {
+        console.log("Dins del sumatori")
+        console.log(valor, index, llista)
+        total = total + valor
+    }
 
-function sumatori(valor, index, array) {
-    console.log(valor, index, array)
+    function calcularMitjana(valor) {
+        mitjana = (mitjana + valor) / 2
+    }
+
+    numeros.forEach(calcularMitjana)
+    console.log(mitjana)
 }
-

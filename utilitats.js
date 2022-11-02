c14a183c73196d9a0c586e6b36dfcf990c7f657a
SuleeJK <silemanijangana@gmail.com>
2022-11-02 17:30:35 +0100

    S'ha afegit el codi js del index.html

---
+++ utilitats.js
@@ -0,0 +1,57 @@
+
+console.log(prova())
+
+function prova(){
+    console.log("hola que tal")
+} 
+
+// var elMeuNom = "Silemani"
+    // console.log("El contingut de la variable és " + elMeuNom + " i el tipus de la variable és", typeof elMeuNom)
+
+    // var laMevaEdat = "21"
+    // elTipusDeVariableDeLaMevaEdat = typeof laMevaEdat
+
+    // console.log("Contingut", laMevaEdat)
+    // console.log("Tipus", elTipusDeVariableDeLaMevaEdat)
+
+    // var edat = 21
+    // console.log(edat, typeof edat)
+    // var edat2030 = edat + 8
+    // console.log("Al 2030 tindré ", edat2030, " anys.")
+
+    // function suma(a,b) {
+        
+    //     return a + b
+    // }
+
+    // suma(10,20)
+    // console.log("La suma és", suma(10,20))
+
+    function mostraInformacio(laVariable)
+    {
+        console.log("Contingut: ", laVariable)
+        console.log("Tipus: ", typeof(laVariable))
+    }
+    
+    // var variable1 = "Silemani"
+    // var variable2 = 13
+    // var variable3 = false
+
+    // hola = mostraInformacio(variable1)
+    // console.log(hola)
+    
+
+    // const nom2 = prompt("Com et dius?")
+    // console.log("El teu nom és", nom2)
+    // console.log(document.getElementById("input"))
+
+    // var noms = ["jordi", "pere", "laura", 35]
+    // mostraInformacio(noms)
+    
+    // if (Array.isArray(noms)) {
+
+    //     console.log("És un array")      
+    // }
+    // else{ 
+    //     console.log("És un array")  
+    // }
